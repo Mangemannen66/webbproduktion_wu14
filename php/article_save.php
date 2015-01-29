15ef6c094e3784cb05d60cfabb1032ee9c06277b
Mangemannen66 <magnus@md-produktion.se>
2015-01-29 19:41:33 +0100

    ///

--- php/article_save.php
+++ php/article_save.php
@@ -6,6 +6,6 @@ $query = New Articlequery("127.0.0.1","webbproduktion_wu14","root","mysql");
 
 //save content if told to do so (by receiving correct AJAX data)
 if (isset($_REQUEST["page_data"])) {
-  //save page and echo ContentQueries response
+  //save page and echo articlequery response
   echo(json_encode($query->saveNewArticle($_REQUEST["page_data"])));
 }
--- php/classes/articlequery.class.php
+++ php/classes/articlequery.class.php
@@ -9,10 +9,7 @@ class Articlequery extends PDOHelper {
     $page_data[":user_id"] = $this->user_info["user_id"];
 
     $sql = "INSERT INTO pages (title, content, user_id) VALUES (:title, :content, :user_id)";
-    //since we are using prepared SQL statements, 
-    //SQL and data are sent separately to the query method
 
-    //first insert into the pages table
     $this->query($sql, $page_data);
 
 
